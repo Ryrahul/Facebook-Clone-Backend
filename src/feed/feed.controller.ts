4042b8e537ab2aea9ec4c250f541b3266ad49077
Rahul <caulirahul6@gmail.com>
2023-12-24 20:59:01 +0530

    fix: Router typo

--- src/feed/feed.controller.ts
+++ src/feed/feed.controller.ts
@@ -1,9 +1,10 @@
-import { Controller, Req } from '@nestjs/common';
+import { Controller, Get, Req } from '@nestjs/common';
 import { FeedService } from './feed.service';
 
 @Controller('feed')
 export class FeedController {
     constructor(private readonly feedService:FeedService){}
+    @Get()
     async getFeed(@Req() req){
         return await this.feedService.getFeed(req.user.id)
 
